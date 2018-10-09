52d7f57fdaded491807d8f37e9b4012c73ee82d0
slcnx <1062670898@qq.com>
2018-10-09 14:35:47 -0400

    ss up

---
+++ ss/README.txt
@@ -0,0 +1,5 @@
+ss 
+sslog
+dir must be exist.
+
+crontab: * * * * * /bin/bash /root/ss/check_22.sh > /dev/null
---
+++ ss/ss/check_22.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin
+port=22
+
+rm -f /root/ss/$port-tmp1 /root/ss/$port-tmp2
+for i in /root/sslog/${port}*; do tcpdump -r  $i | grep -Po '(\d+\.){3}\d+' | sort -u >> /root/ss/$port-tmp1; done
+sort -u /root/ss/$port-tmp1 > /root/ss/$port-tmp2
+sort -u /root/ss/$port-tmp2 /root/ss/$port-ip-access.log  > /root/ss/$port-ip-access.log
+
+whole_name=`ls -lth /root/sslog/* | grep -v '^total' |awk '{print $NF}' | head -n 1`
+find /root/sslog/ -mindepth 1 -not -wholename "$whole_name" -delete
+
+rm -f /root/ss/$port-tmp1 /root/ss/$port-tmp2
